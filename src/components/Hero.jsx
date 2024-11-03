573fee225c4a2c1d8d33f33ede7a81da9473b41d
unknown <medisaagum@.gmail.com>
2024-11-03 21:33:19 +0700

    penambahan style pada Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,18 +2,20 @@ import React from "react";
 
 export default function Hero(){
     return(
-        <div className="min-h-screen flex flex-col gap-3 items-center justify-center
-        text-center max-w-[900px] w-full mx-auto" >
-        <div className="flex flex-col gap-4">
-            <p>SAATNYA UNTUK MENGGUNAKAN</p>
-            <h1 className="uppercase font-bold text-5xl 
+        <div className="min-h-screen flex flex-col gap-8 items-center justify-center
+        text-center max-w-[900px] w-full mx-auto p-4" >
+        <div className="flex flex-col gap-2">
+            <p>SAATNYA UNTUK MENCOBA</p>
+            <h1 className="font-bold text-5xl 
             sm:text-6xl md:text-7xl">
-                <span className="text-yellow-200">My</span>Gym</h1>
+                <span className="text-green-500">My</span>GYM</h1>
         </div>
             <p className="text-sm md:text-base font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Facilis, <span className="text-yellow-100 font-medium">Lorem, ipsum dolor.</span> harum similique quo numquam laborum mollitia!</p>
+            Facilis, <span className="text-green-500 font-medium">Lorem, ipsum dolor.</span> harum similique quo numquam laborum mollitia!</p>
 
-            <button className="px-8 py-4 rounded-md border-[2px] bg-slate border-yellow-300 border-solid ">Masuk & Dapatkan</button>
+            <button className="px-6 py-3 rounded-md border-[2px] bg-slate-95 border-green-300 border-solid greenShadow duration-300 ">
+                Masuk & Dapatkan
+            </button>
         </div>
     )
 }
