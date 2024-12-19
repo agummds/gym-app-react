082063771d68ebbbdd23e46add51ed2e2fa5b1fd
unknown <medisaagum@.gmail.com>
2024-12-19 21:26:05 +0700

    add some style & statement

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import SectionWrapper from "./SectionWrapper";
 
+function Header(props) {
+  const { index, title, description } = propsreturn(
+    <div>
+      <div>
+        <p>{index}</p>
+        <h4>{title}</h4>
+      </div>
+      <p>{description}</p>
+    </div>
+  );
+}
+
 export default function Generator() {
   return (
     <SectionWrapper
       header={"generate our work out"}
-      title={["it\'s", "Huge", "o\'clock"]}>
-        hahaha
-      </SectionWrapper>
+      title={["it's", "Huge", "o'clock"]}
+    >
+      <Header
+        index={"01"}
+        title={"pick your poison"}
+        description={"Select the workout you wish to endure."}
+      />
+    </SectionWrapper>
   );
 }
